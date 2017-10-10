0d9853f2d6d6b471f14528580c192b4ea4002e0d
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2017-10-10 15:59:14 +0200

    feat: use mv instead of cp

--- run.sh
+++ run.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 if [ $KBC_PARAMETER_DIRECTION == "files" ]; then
     cd $KBC_DATADIR/in/tables/
-    find . ! -iname "*.manifest" ! -name "." | xargs -n1 -I {} sh -c "cp \"{}\" $KBC_DATADIR/out/files/\"{}\""
+    find . ! -iname "*.manifest" ! -name "." | xargs -n1 -I {} sh -c "mv \"{}\" $KBC_DATADIR/out/files/\"{}\""
 fi
 
 if [ $KBC_PARAMETER_DIRECTION == "tables" ]; then
     [ -z "$KBC_PARAMETER_ADDCSVSUFFIX" ] && export KBC_PARAMETER_ADDCSVSUFFIX=0;
     cd $KBC_DATADIR/in/files/
     if [ $KBC_PARAMETER_ADDCSVSUFFIX == "true" ] || [ $KBC_PARAMETER_ADDCSVSUFFIX == "1" ]; then
-        find . ! -iname "*.manifest" ! -name "." | xargs -n1 -I {} sh -c "cp \"{}\" $KBC_DATADIR/out/tables/\"{}\".csv"
+        find . ! -iname "*.manifest" ! -name "." | xargs -n1 -I {} sh -c "mv \"{}\" $KBC_DATADIR/out/tables/\"{}\".csv"
         exit 0
     else
-        find . ! -iname "*.manifest" ! -name "." | xargs -n1 -I {} sh -c "cp \"{}\" $KBC_DATADIR/out/tables/\"{}\""
+        find . ! -iname "*.manifest" ! -name "." | xargs -n1 -I {} sh -c "mv \"{}\" $KBC_DATADIR/out/tables/\"{}\""
     fi
 fi
 
